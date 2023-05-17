11bd17f8605447a3d95d214f145493e7b8bcae2a
Zahro03 <127277232+Zahro03@users.noreply.github.com>
2023-05-17 18:21:55 +0700

    menampilkan bukunya.setJudul Matematika

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -18,7 +18,7 @@ public:
 
 int main()
 {
-	//bukunya.setJudul("Matematika");
+	//bukunya.setJudul("Matematika"); 
 	//Cout << bukunya.getJudul();
 	cout << bukunya.setJudul("Matematika").getJudul(); //chain function calls
 	return 0;
